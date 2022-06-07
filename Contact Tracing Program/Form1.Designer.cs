116b7ebc31cdf5de91c127a413c459626c5cc39e
Sier Santos <louisasantosl12@gmail.com>
2022-06-07 23:18:32 +0900

    Restored the bg

--- Contact Tracing Program/Form1.Designer.cs
+++ Contact Tracing Program/Form1.Designer.cs
@@ -1650,6 +1650,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(1354, 733);
             this.Controls.Add(this.CTbtnClear);
             this.Controls.Add(this.CTbtnSave);
