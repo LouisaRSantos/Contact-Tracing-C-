509a27145b67c128fad6d20388c85d00dc7fd040
Sier Santos <louisasantosl12@gmail.com>
2022-06-07 22:13:15 +0900

    Added second in the list

--- Contact Tracing Program/Form1.cs
+++ Contact Tracing Program/Form1.cs
@@ -58,6 +58,7 @@ namespace Contact_Tracing_Program
             {
                 try
                 {
+                    //About1
                     File.WriteAllText(CTsaveFD.FileName, CTlabelAbout1.Text + "\r\n");
                     File.AppendAllText(CTsaveFD.FileName, CTlabelName.Text + TxtBoxFN.Text + " " + TxtBoxMN.Text + " " + TxtBoxSN.Text + " " + TxtBoxSuffix.Text + "\r\n");
                     File.AppendAllText(CTsaveFD.FileName, CTlabelAge.Text + TxtBoxAge.Text + " years old" + "\r\n");
@@ -68,6 +69,7 @@ namespace Contact_Tracing_Program
                     File.AppendAllText(CTsaveFD.FileName, CTlabelBirthPlace.Text + TxtBoxBP.Text + "\r\n");
                     File.AppendAllText(CTsaveFD.FileName, CTlabelCurrentAddress.Text + TxtBoxCA.Text + "\r\n" + "\r\n");
 
+                    //About2
                     File.AppendAllText(CTsaveFD.FileName, CTlabelAbout2.Text + "\r\n");
                     File.AppendAllText(CTsaveFD.FileName, CTlabelMother.Text + TxtBoxMotherFN.Text + " " + TxtBoxMotherMN.Text + " " + TxtBoxMotherSN.Text + " " + TxtBoxMotherSuffix.Text + "\r\n");
                     File.AppendAllText(CTsaveFD.FileName, CTlabelMotherPNumber.Text + TxtBoxMotherPN.Text + "\r\n");
@@ -76,24 +78,34 @@ namespace Contact_Tracing_Program
                     File.AppendAllText(CTsaveFD.FileName, CTlabelFatherPNumber.Text + TxtBoxFatherPN.Text + "\r\n");
                     File.AppendAllText(CTsaveFD.FileName, CTlabelFatherCA.Text + TxtBoxFatherCA.Text + "\r\n" + "\r\n");
 
+                    //About3
                     File.AppendAllText(CTsaveFD.FileName, CTlabelAbout3.Text + "\r\n");
                     File.AppendAllText(CTsaveFD.FileName, "1. " + CTlabelQ1.Text + "- " + "\r\n");
                     File.AppendAllText(CTsaveFD.FileName, "\t" + CTcheckboxYes1.Text + ": " + CTcheckboxYes1.Checked.ToString() + " " + CTcheckbocNo1.Text + ": " + CTcheckbocNo1.Checked.ToString() + "\r\n");
                     File.AppendAllText(CTsaveFD.FileName, "2. " + CTlabelQ2.Text + "- " + "\r\n");
                     File.AppendAllText(CTsaveFD.FileName, "\t" + CTcheckboxYes2.Text + ": " + CTcheckboxYes2.Checked.ToString() + " " + CTcheckboxNo2.Text + ": " + CTcheckboxNo2.Checked.ToString() + "\r\n" + "\r\n");
 
+                    //About4
                     File.AppendAllText(CTsaveFD.FileName, CTlabelAbout4.Text + "\r\n");
                     File.AppendAllText(CTsaveFD.FileName, "3. " + CTlabelQ3.Text + "- " + "\r\n" );
                     File.AppendAllText(CTsaveFD.FileName, "\t" + CTcheckboxYes3.Text + ": " + CTcheckboxYes3.Checked.ToString() + " " + CTcheckboxNo3.Text + ": " + CTcheckboxNo3.Checked.ToString() + "\r\n");
                     File.AppendAllText(CTsaveFD.FileName, "4. " + CTlabelQ4.Text + "- " + "\r\n");
                     File.AppendAllText(CTsaveFD.FileName, "\t" + CTcheckboxYes4.Text + ": " + CTcheckboxYes4.Checked.ToString() + " " + CTcheckboxNo4.Text + ": " + CTcheckboxNo4.Checked.ToString() + " " + CTcheckboxIDK1.Text + ": " + CTcheckboxIDK1.Checked.ToString() + "\r\n" + "\r\n");
 
+                    //About5.1
                     File.AppendAllText(CTsaveFD.FileName, CTlabelAbout5.Text + "\r\n");
                     File.AppendAllText(CTsaveFD.FileName, "1. " + "\r\n");
                     File.AppendAllText(CTsaveFD.FileName, "\t" + CTlabelName1.Text + TxtBoxName1.Text + "\r\n");
                     File.AppendAllText(CTsaveFD.FileName, "\t" + CTlabelDate1.Text + TxtBoxDate1.Text + "\r\n");
                     File.AppendAllText(CTsaveFD.FileName, "\t" + CTlabelTime1.Text + TxtBoxTime1.Text + "\r\n");
                     File.AppendAllText(CTsaveFD.FileName, "\t" + CTlabelLoc1.Text + TxtBoxLoc1.Text + "\r\n");
+
+                    //About5.2
+                    File.AppendAllText(CTsaveFD.FileName, "2. " + "\r\n");
+                    File.AppendAllText(CTsaveFD.FileName, "\t" + CTlabelName2.Text + TxtBoxName2.Text + "\r\n");
+                    File.AppendAllText(CTsaveFD.FileName, "\t" + CTlabelDate2.Text + TxtBoxDate2.Text + "\r\n");
+                    File.AppendAllText(CTsaveFD.FileName, "\t" + CTlabelTime2.Text + TxtBoxTime2.Text + "\r\n");
+                    File.AppendAllText(CTsaveFD.FileName, "\t" + CTlabelLoc2.Text + TxtBoxLoc2.Text + "\r\n");
 
                     MessageBox.Show("File Saved");
                 }
